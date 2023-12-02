1801d4db70f4f2a79ec6a928da488edffc9a7666
Zenuls <ivanov694420@gmail.com>
2023-12-02 15:26:26 +1000

    Добавлены множественное наследование и абстактные классы

--- Human.cpp
+++ Human.cpp
@@ -52,6 +52,56 @@ class Human_with_city_of_birth : public Human {
     }
 };
 
+class University {
+    private:
+    string name_of_university{"ДВФУ"};
+    public:
+    University() {}
+    University(string name_of_university): name_of_university(name_of_university) {}
+    string Get_name_of_university() {
+        return this->name_of_university;
+    }
+    void Set_name_of_university(const string u) {
+        name_of_university = u;
+    }
+};
+
+class Specialization {
+    private:
+    string number{"09.03.04 Программная инженерия"};
+    public:
+    Specialization() {}
+    Specialization(string number): number(number) {}
+    string Get_number() {
+        return this->number;
+    }
+    void Set_number(const string nu) {
+        number = nu;
+    }
+};
+
+class Student : public Human_with_city_of_birth, virtual public University, Specialization {
+    private:
+    unsigned short int course{2};
+    public:
+    Student() {}
+    Student(string f_name, string s_name, unsigned int age, bool gender, string city, string name_of_university, string number, unsigned short int course): Human_with_city_of_birth (f_name, s_name, age, gender, city), University(name_of_university), Specialization(number), course(course) {}
+    unsigned short int Get_course() {
+        return this->course;
+    }
+    void Set_course(const unsigned short int c) {
+        course = c;
+    }
+    void Presentation() {
+        cout <<"Привет! Меня зовут "<<this->Get_f_name()<<" "<<this->Get_s_name()<<endl;
+        cout <<"Мне "<<this->Get_age()<<" лет"<<endl;
+        cout <<"Я родился/родилась в городе: "<<this->Get_city()<<endl;
+        cout <<"И учусь в "<<this->Get_name_of_university()<<" по професии "<<this->Get_number()<<" на "<<this->Get_course()<<" курсе"<<endl;
+        cout <<endl;
+    }
+
+};
+
 int main() {
     return 0;
 }
